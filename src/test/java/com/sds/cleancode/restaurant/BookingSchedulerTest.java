2c3c6b694767499e1083b5f95fdaa7d48261eb1c
nubang <nubang7@gmail.com>
2019-02-06 18:42:32 +0900

    Step1. 예약은 정시에만 가능하다. 정시가 아닌경우 예외발생.

--- src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -4,12 +4,24 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class BookingSchedulerTest {
 
-	@Test
-	public void testName() throws Exception {
-		fail();
+	@Test(expected = RuntimeException.class)
+	public void Step1_예약은_정시에만_가능하다_정시가_아닌경우_예외발생() {
+		
+		// arrange
+		DateTime notOnTheHour= new DateTime(2017, 2, 6, 17, 5);
+		Customer customer= new Customer("Fake name", "010-1234-4727");
+		Schedule schedule= new Schedule(notOnTheHour, 1, customer);
+		BookingScheduler bookingScheduler= new BookingScheduler(3);
+		
+		// act
+		bookingScheduler.addSchedule(schedule);
+		
+		// assert
+		// expected runtime exception
 	}
 }
