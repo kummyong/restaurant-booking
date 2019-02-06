d80ecad1ad2a7c1eb3096f3eaaade2902a662dda
nubang <nubang7@gmail.com>
2019-02-06 18:54:00 +0900

    Step3. 테스트 코드 리팩토링

--- src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -10,15 +10,20 @@ import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 public class BookingSchedulerTest {
-
+	
+	private static final DateTimeFormatter DATE_TIME_FORMATTER= DateTimeFormat.forPattern("YYYY/MM/dd HH:mm");
+	private static final DateTime ON_THE_HOUR= DATE_TIME_FORMATTER.parseDateTime("2017/06/19 17:00");
+	private static final DateTime NOT_ONT_THE_HOUR= new DateTime(2017, 2, 6, 17, 5);
+	private static final Customer CUSTOMER= new Customer("Fake name", "010-1234-4727");
+	private static final int MAX_CAPACITY = 3;
+	private static final int UNDER_CAPACITY = 1;
+	private BookingScheduler bookingScheduler= new BookingScheduler(MAX_CAPACITY);
+	
 	@Test(expected = RuntimeException.class)
 	public void Step1_예약은_정시에만_가능하다_정시가_아닌경우_예외발생() {
 		
 		// arrange
-		DateTime notOnTheHour= new DateTime(2017, 2, 6, 17, 5);
-		Customer customer= new Customer("Fake name", "010-1234-4727");
-		Schedule schedule= new Schedule(notOnTheHour, 1, customer);
-		BookingScheduler bookingScheduler= new BookingScheduler(3);
+		Schedule schedule= new Schedule(NOT_ONT_THE_HOUR, UNDER_CAPACITY, CUSTOMER);
 		
 		// act
 		bookingScheduler.addSchedule(schedule);
@@ -31,11 +36,7 @@ public class BookingSchedulerTest {
 	public void Step2_예약은_정시에만_가능하다_정시인_경우_스케줄_추가_성공() {
 		
 		// arrange
-		DateTimeFormatter dateTimeFormatter= DateTimeFormat.forPattern("YYYY/MM/dd HH:mm");
-		DateTime onTheHour= dateTimeFormatter.parseDateTime("2017/06/19 17:00");
-		Customer customer= new Customer("Fake name", "010-1234-4727");
-		Schedule schedule= new Schedule(onTheHour, 1, customer);
-		BookingScheduler bookingScheduler= new BookingScheduler(3);
+		Schedule schedule= new Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER);
 		
 		// act
 		bookingScheduler.addSchedule(schedule);
@@ -43,4 +44,6 @@ public class BookingSchedulerTest {
 		// assert
 		assertThat(bookingScheduler.hasSchedule(schedule), is(true));
 	}
+	
+	// Step3. 테스트 코드 리팩토링
 }
